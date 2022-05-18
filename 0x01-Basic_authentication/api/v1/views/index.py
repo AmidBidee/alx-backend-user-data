94e807b557d4dd1cfd81f2ed46e8c0504b2ef491
krummitz <amidbidee@gmail.com>
2022-05-18 03:58:17 +0100

    updates

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,12 @@ def unauthorized_endpoint():
     """ An unauthorized endpoint
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Forbidden error.
+    """
+    abort(403)
