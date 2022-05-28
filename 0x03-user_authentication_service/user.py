ee872241db2d72c2aff9aa9458b7d1f187812ce7
krummitz <amidbidee@gmail.com>
2022-05-28 03:24:44 +0000

    updates

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,37 +1,19 @@
 #!/usr/bin/env python3
-"""user module
+"""The `user` model's module.
 """
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (
-        Column,
-        Integer,
-        String
-        )
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """A user class model defining the following
-    attributes.
-
-    Attributes:
-    id, the integer primary key
-    email, a non-nullable string
-    hashed_password, a non-nullable string
-    session_id, a nullable string
-    reset_token a nullable string
+    """Represents a record from the `user` table.
     """
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self) -> str:
-        """A string reprisenation of the User
-        model.
-        """
-        return "id: %s, email: %s".format(id, email)
